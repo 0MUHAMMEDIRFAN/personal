192bb75c36fb12907e67f0c333c7e0969a537b74
Muhammed irfan pc <irfannadapuram@gmail.com>
2025-04-23 00:47:48 +0530

    updated little things like experience and projects ui copied from adrijoe yt

--- src/Sections/Experience.tsx
+++ src/Sections/Experience.tsx
@@ -1,47 +1,115 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-
+import { motion } from 'framer-motion'
 import "react-vertical-timeline-component/style.min.css";
 
 import contents from '../../contents.json';
 
-const ExperienceCard: React.FC<{ title: string, company: string, duration: string, tenure: string, description: string[], icon?: string }> = ({ title, company, duration, tenure, description, icon }) => (
-    <VerticalTimelineElement
-        contentStyle={{ background: "#ff00ff", color: '#fff' }}
-        contentArrowStyle={{ borderRight: "solid 1px #000" }}
-        date={duration}
-        iconStyle={{}}
-        icon={""}
-    >
-        <div>
-            {title}
-            {company}
-            {tenure}
-            {description}
-            {icon}
-        </div>
-    </VerticalTimelineElement>
-)
+
+const ExperienceCard = ({ title, company, duration, tenure, description, icon }) => {
+    return (
+        <VerticalTimelineElement
+            contentStyle={{
+                background: "#093820",
+                color: "#fff",
+            }}
+            contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+            date={tenure}
+            iconStyle={{ background: "#000" }}
+            icon={
+                <div className='flex text-[#1d1836] justify-center items-center w-full h-full rounded-full overflow-hidden'>
+                    <img
+                        src={icon}
+                        alt={company}
+                        className='w-[100%] h-[100%] object-contain'
+                    />
+                </div>
+            }
+        >
+            <div>
+                <h3 className='text-white text-[24px] font-bold'>{title}</h3>
+                <p
+                    className='text-secondary text-[16px] font-semibold'
+                    style={{ margin: 0 }}
+                >
+                    {company}
+                </p>
+            </div>
+
+            <ul className='mt-5 list-disc ml-5 space-y-2'>
+                {description.map((point, index) => (
+                    <li
+                        key={`experience-point-${index}`}
+                        className='text-white-100 text-[14px] pl-1 tracking-wider'
+                    >
+                        {point}
+                    </li>
+                ))}
+            </ul>
+        </VerticalTimelineElement>
+    );
+};
+
+// const ExperienceCard: React.FC<{ title: string, company: string, duration: string, tenure: string, description: string[], icon?: string }> = ({ title, company, duration, tenure, description, icon }) => (
+//     <VerticalTimelineElement
+//         contentStyle={{ background: "#ff00ff", color: '#fff' }}
+//         contentArrowStyle={{ borderRight: "solid 1px #000" }}
+//         date={duration}
+//         iconStyle={{}}
+//         icon={""}
+//     >
+//         <div>
+//             {title}
+//             {company}
+//             {tenure}
+//             {description}
+//             {icon}
+//         </div>
+//     </VerticalTimelineElement>
+// )
+const textVariant = (delay) => {
+    return {
+        hidden: {
+            y: -50,
+            opacity: 0,
+        },
+        show: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                type: "spring",
+                duration: 1.25,
+                delay: delay,
+            },
+        },
+    };
+};
 
 const Experience: React.FC = () => {
     return (
         <section className="" id="">
+            <motion.div variants={textVariant()}>
+                <p className={"text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] text-center"}>
+                    What I have done so far
+                </p>
+                <h2 className={`text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center`}>
+                    Work Experience.
+                </h2>
+            </motion.div>
             <div className="">
-                <h1 className="">Experience</h1>
-                <div className="">
-                    <VerticalTimeline>
-                        {contents.experience.map((exp, index) => (
-                            <ExperienceCard key={index}
-                                title={exp.role}
-                                company={exp.company}
-                                description={exp.responsibilities}
-                                duration={exp.duration}
-                                tenure={exp.tenure}
-                            />
-                        ))}
-                    </VerticalTimeline>
+                <VerticalTimeline>
+                    {contents.experience.map((exp, index) => (
+                        <ExperienceCard key={index}
+                            title={exp.role}
+                            company={exp.company}
+                            description={exp.responsibilities}
+                            duration={exp.duration}
+                            tenure={exp.tenure}
+                            icon={exp.icon}
+                        />
+                    ))}
+                </VerticalTimeline>
 
-                </div>
             </div>
         </section>
     );
--- src/Sections/Projects.tsx
+++ src/Sections/Projects.tsx
@@ -1,18 +1,159 @@
 import React from 'react';
 import Card from '../Components/Card';
-import { projects } from '../../contents.json'
+import contents from '../../contents.json';
 import { SectionWrapper } from '../hoc';
+import { motion } from 'framer-motion'
+
+const fadeIn = (direction, type, delay, duration) => {
+    return {
+        hidden: {
+            x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+            y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+            opacity: 0,
+        },
+        show: {
+            x: 0,
+            y: 0,
+            opacity: 1,
+            transition: {
+                type: type,
+                delay: delay,
+                duration: duration,
+                ease: "easeOut",
+            },
+        },
+    };
+};
+
+const textVariant = (delay) => {
+    return {
+        hidden: {
+            y: -50,
+            opacity: 0,
+        },
+        show: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                type: "spring",
+                duration: 1.25,
+                delay: delay,
+            },
+        },
+    };
+};
+
+
+const ProjectCard = ({
+    index,
+    name,
+    description,
+    technologies,
+    image,
+    source_code_link,
+}) => {
+    return (
+        <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+            {/* <Tilt
+                options={{
+                    max: 45,
+                    scale: 1,
+                    speed: 450,
+                }}
+                className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+                > */}
+            <div className='bg-primary-alt p-5 rounded-2xl sm:w-[360px] w-full'>
+
+                <div className='relative w-full h-[230px]'>
+                    <img
+                        src={image}
+                        alt='project_image'
+                        className='w-full h-full object-cover rounded-2xl'
+                    />
+
+                    <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+                        <div
+                            onClick={() => window.open(source_code_link, "_blank")}
+                            className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+                        >
+                            <img
+                                src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6DXuvit57V1DmjnlLcwst4O-sTL5D37gIQ&s"}
+                                alt='source code'
+                                className='w-1/2 h-1/2 rounded-full object-contain'
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='mt-5'>
+                    <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+                    <p className='mt-2 text-secondary text-[14px] h-40 overflow-auto'>
+                        {description.map((point, index) => (
+                            <li
+                                key={`experience-point-${index}`}
+                                className='text-white-100 text-[14px] pl-1 tracking-wider'
+                            >
+                                {point}
+                            </li>
+                        ))}
+                    </p>
+                </div>
+
+                <div className='mt-4 flex flex-wrap gap-x-2'>
+                    {technologies.map((technology) => (
+                        <p
+                            key={`${name}-${technology}`}
+                            className={`text-[14px]`}
+                        >
+                            #{technology}
+                        </p>
+                    ))}
+                </div>
+            </div>
+
+            {/* </Tilt> */}
+        </motion.div>
+    );
+};
 
 const Projects: React.FC = () => {
     return (
-        <div className="flex flex-col items-center gap-5">
-            <h1 className="text-3xl font-semibold">Projects</h1>
-            <div className="flex justify-start gap-10 w-full overflow-x-scroll">
-                {projects.map((project, index) => {
-                    return <Card key={index} heading={project.name} descriptions={project.description} />
-                })}
+        // <div className="flex flex-col items-center gap-5">
+        //     <h1 className="text-3xl font-semibold">Projects</h1>
+        //     <div className="flex justify-start gap-10 w-full overflow-x-scroll">
+        //         {projects.map((project, index) => {
+        //             return <Card key={index} heading={project.name} descriptions={project.description} />
+        //         })}
+        //     </div>
+        // </div>
+
+        <>
+            <motion.div variants={textVariant()}>
+                <p className={"text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] text-center"}>
+                    My work</p>
+                <h2 className={`text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center`}>
+                    Projects.</h2>
+            </motion.div>
+
+            <div className='w-full flex justify-center text-center'>
+                <motion.p
+                    variants={fadeIn("", "", 0.1, 1)}
+                    className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+                >
+                    Following projects showcases my skills and experience through
+                    real-world examples of my work. Each project is briefly described with
+                    links to code repositories and live demos in it. It reflects my
+                    ability to solve complex problems, work with different technologies,
+                    and manage projects effectively.
+                </motion.p>
             </div>
-        </div>
+
+            <div className='mt-20 flex flex-wrap gap-7 justify-center'>
+                {contents.projects.map((project, index) => (
+                    <ProjectCard key={`project-${index}`} index={index} {...project} />
+                ))}
+            </div>
+        </>
     );
 };
 
